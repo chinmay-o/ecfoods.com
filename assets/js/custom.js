13def93ae4f17dc33b342fa4127aef543555fe5e
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-12-09 23:07:04 +0530

    beta V1.1.1
    98% updated

---
+++ assets/js/custom.js
@@ -0,0 +1,42 @@
+
+$("#quality-scroll-down").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".quality-area").offset().top - 60
+      }, 1000);
+});
+
+$("#slider-product-button01").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".products-area").offset().top - 60
+      }, 1000);
+});
+
+$("#slider-contact-button01").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".contact-area").offset().top - 60
+      }, 1000);
+});
+
+$("#slider-product-button02").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".products-area").offset().top - 60
+      }, 1000);
+});
+
+$("#slider-contact-button02").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".contact-area").offset().top - 60
+      }, 1000);
+});
+
+$("#testimonial-footer").click(function() {
+
+    $('html, body').animate({
+        scrollTop: $(".testimonial-area").offset().top - 60
+      }, 1000);
+});
